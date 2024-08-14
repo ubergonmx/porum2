5357f13edefa880ac0f7b37696d486ee3c91cd86
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-08-15 04:13:37 +0800

    Revert adding auth and admin pages

--- src/app/(auth)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from "react";
-
-export default function AuthLayout({ children }: { children: ReactNode }) {
-  // pt:mt-0 mx-auto flex flex-col items-center justify-center px-6 pt-8 dark:bg-gray-900 md:h-screen
-  return <div className="grid h-full place-items-center p-4">{children}</div>;
-}
