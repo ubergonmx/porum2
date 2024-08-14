b0768437f1a5dd00f386145b6548ba9f9d608df7
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-08-15 01:31:10 +0800

    Update next.config.mjs, and add .github, .husky, and .vscode files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ["@node-rs/argon2"],
+    },
+    images: {
+    remotePatterns: [
+        {
+        protocol: "https",
+        hostname: "utfs.io",
+        // pathname: `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/*`,
+        },
+    ],
+    },
+};
 
 export default nextConfig;
