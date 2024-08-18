80dc61366de19cfe9acc370a8dba59adb5ac2f66
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-08-18 19:57:08 +0800

    Fix object-fit of avatar on header

--- src/components/user-avatar.tsx
+++ src/components/user-avatar.tsx
@@ -26,6 +26,7 @@ export function UserAvatar({
             src={avatarURL}
             alt="profile picture"
             referrerPolicy="no-referrer"
+            className="object-cover"
           />
         </div>
       ) : (
